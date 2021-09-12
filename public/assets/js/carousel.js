61f07605b41daedffb5e517dfd4e7309648ab29f
Carlo <ccarlo@hotmail.it>
2021-09-12 15:48:21 +0200

    Done

---
+++ public/assets/js/carousel.js
@@ -0,0 +1,5 @@
+new Splide( '.splide', {
+	type   : 'loop',
+	perPage: 1,
+	width: '100%'
+} ).mount();
---
+++ public/financement.php
@@ -0,0 +1,25 @@
+<?php
+
+require_once (__DIR__ . '/includes/header.php');
+
+?>
+
+<section id="forum-header">
+	<img src="<?=$domain?>/icons/FLECHE BAS BLEUE.svg" alt="">
+	<img src="<?=$domain?>/icons/ROND ORANGE.svg" alt="">
+	<div class="forum-bg">
+		<div class="container">
+			<div class="forum-header">
+				<div class="forum-text">
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+
+<?php
+
+require_once (__DIR__ . '/includes/footer.php');
+
+?>
