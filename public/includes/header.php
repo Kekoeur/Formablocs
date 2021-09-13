62e7b899de4d26cbd44e547ab6755570839f3a78
Christo26.g <Christo26.g@gmail.com>
2021-09-13 21:58:20 +0200

    login with modal

--- public/includes/header.php
+++ public/includes/header.php
@@ -16,13 +16,6 @@ require_once (__DIR__ . '../../db/global_db.php');
 	<!-- ================= CSS ================== -->
 	<link rel="stylesheet" href="<?=$domain?>/css/style.css">
 	<link rel="stylesheet" href="https://use.typekit.net/afv1gmg.css">
-
-	<!-- ================ SCRIPTS ================ -->
-	<script src="<?=$domain?>/js/script.js"></script>
-
-	<!-- ================ JQUERY ================= -->
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-
 </head>
 <body>
 
