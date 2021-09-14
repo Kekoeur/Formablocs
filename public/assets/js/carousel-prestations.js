9e692f1d6471ce0c4702dcc1d170b18ef2d54b88
Carlo <ccarlo@hotmail.it>
2021-09-15 00:07:26 +0200

    ooo

---
+++ public/assets/js/carousel-prestations.js
@@ -0,0 +1,124 @@
+let slidePosition = 0;
+let slidePosition2 = 0;
+let slidePosition3 = 0;
+
+const slides = document.getElementsByClassName('prestation-slide');
+const slides2 = document.getElementsByClassName('prestation2-slide');
+const slides3 = document.getElementsByClassName('prestation3-slide');
+
+const totalSlides = slides.length;
+const totalSlides2 = slides2.length;
+const totalSlides3 = slides3.length;
+
+const prevOne = document.getElementById('prev-1');
+const nextOne = document.getElementById('next-1');
+
+const prevTwo = document.getElementById('prev-2');
+const nextTwo = document.getElementById('next-2');
+
+const prevThree = document.getElementById('prev-3');
+const nextThree = document.getElementById('next-3');
+
+nextOne.addEventListener('click', () => {
+	toNextSlide();
+})
+
+prevOne.addEventListener('click', () => {
+	toPrevSlide();
+})
+
+nextTwo.addEventListener('click', () => {
+	toNextSlide2();
+})
+
+prevTwo.addEventListener('click', () => {
+	toPrevSlide2();
+})
+
+nextThree.addEventListener('click', () => {
+	toNextSlide3();
+})
+
+prevThree.addEventListener('click', () => {
+	toPrevSlide3();
+})
+
+function updateSlidePosition() {
+	for(let slide of slides){
+		slide.classList.remove('current-slide');
+		slide.classList.add('hidden-slide')
+	}
+	slides[slidePosition].classList.add('current-slide');
+	slides[slidePosition].classList.remove('hidden-slide');
+}
+
+function updateSlidePosition2() {
+	for(let slide of slides2){
+		slide.classList.remove('current-slide');
+		slide.classList.add('hidden-slide')
+	}
+	slides2[slidePosition2].classList.add('current-slide');
+	slides2[slidePosition2].classList.remove('hidden-slide');
+}
+
+function updateSlidePosition3() {
+	for(let slide of slides3){
+		slide.classList.remove('current-slide');
+		slide.classList.add('hidden-slide')
+	}
+	slides3[slidePosition3].classList.add('current-slide');
+	slides3[slidePosition3].classList.remove('hidden-slide');
+}
+
+function toNextSlide() {
+	if(slidePosition === totalSlides - 1){
+		slidePosition = 0;
+	} else {
+		slidePosition++;
+	}
+	updateSlidePosition();
+}
+
+function toPrevSlide() {
+	if(slidePosition === 0){
+		slidePosition = totalSlides - 1;
+	} else {
+		slidePosition--;
+	}
+	updateSlidePosition();
+}
+
+function toNextSlide2() {
+	if(slidePosition2 === totalSlides2 - 1){
+		slidePosition2 = 0;
+	} else {
+		slidePosition2++;
+	}
+	updateSlidePosition2();
+}
+
+function toPrevSlide2() {
+	if(slidePosition2 === 0){
+		slidePosition2 = totalSlides2 - 1;
+	} else {
+		slidePosition2--;
+	}
+	updateSlidePosition2();
+}
+function toNextSlide3() {
+	if(slidePosition3 === totalSlides3 - 1){
+		slidePosition3 = 0;
+	} else {
+		slidePosition3++;
+	}
+	updateSlidePosition3();
+}
+
+function toPrevSlide3() {
+	if(slidePosition3 === 0){
+		slidePosition3 = totalSlides3 - 1;
+	} else {
+		slidePosition3--;
+	}
+	updateSlidePosition3();
+}
--- public/prestations.php
+++ public/prestations.php
@@ -14,7 +14,7 @@ require_once (__DIR__ . '/includes/header.php');
 <section id="home-ctas">
 	<div class="container">
 		<div class="home-btns">
-			<a href="formations.php"><button class="smaller-text">Formation pour les professionnels de la formation</button></a>
+			<a href="formations.php" class="smaller-text"><button>Formation pour les professionnels de la formation</button></a>
 			<a href="financement.php"><button>Accompagnements</button></a>
 		</div>
 	</div>
@@ -28,64 +28,213 @@ require_once (__DIR__ . '/includes/header.php');
 				<h1>Formations pour les <span>professionnels de la formation</span></h1>
 			</div>
 
+			<img src="<?=$domain?>/illustrations/ILLUSTRATION PRESTATION 1.svg" alt="">
+
 			<div class="prestations-carousel">
 
-				<div class="finance-carousel-left-arrow">
+				<div class="prestation-left-arrow">
 					<img src="<?=$domain?>/icons/FLECHE GAUCHE BLEUE.svg" alt="Précedent" id="prev-1">
 				</div>
 
-				<div class="prestations-card">
+				<div class="prestations-card prestation-slide current-slide">
 					<div class="prestation-card-top">
 							<h3>Digitalisation</h3>
 							<p>Tarif : <span>2 000 € par formation</span></p>
-						<?php if (isset($_SESSION['user_id'])) { ?>
-							<form action="../checkout-session.php?formation=Digitalisation" method="POST">
-								<button type="submit">Acheter cette prestation</button>
-							</form>
-							<form action="../create-quote.php?formation=<?=$row['formation']?>" method="POST">
-								<button type="submit">Obtenir un devis</button>
-							</form>
-						<?php } else { ?>
-							<form action="login-signup.php" method="POST">
-								<button type="submit">Acheter cette prestation</button>
-							</form>
-							<form action="login-signup.php" method="POST">
-								<button type="submit">Obtenir un devis</button>
-							</form>
-						<?php } ?>
+
+							<div class="prestation-top-btns">
+								<?php if (isset($_SESSION['user_id'])) { ?>
+									<form action="../checkout-session.php?link=Digitalisation" method="POST">
+										<button type="submit">Acheter cette prestation</button>
+									</form>
+									<form action="../create-quote.php?link=Digitalisation" method="POST">
+										<button type="submit">Obtenir un devis</button>
+									</form>
+								<?php } else { ?>
+									<form action="login-signup.php" method="POST">
+										<button type="submit">Acheter cette prestation</button>
+									</form>
+									<form action="login-signup.php" method="POST">
+										<button type="submit">Obtenir un devis</button>
+									</form>
+								<?php } ?>
+							</div>
 					</div>
 					<div class="prestation-card-text">
-						<p></p>
+						<p>Avec l’avènement des nouvelle technologie et la démocratisation du télétravail, la digitalisation des formations est devenue incontournable pour les organismes de formation. Au delà d’une réelle demande des apprenants, elle permet d’augmenter la zone d’achalandise de l’organisme et d’augmenter le nombre d’apprenant en formation. Nous vous accompagnons dans la transformation numérique de votre offre en FOAD.</p>
+					</div>
+					<div class="carousel-dots">
+						<span class="active-dot"></span>
+						<span></span>
 					</div>
 				</div>
 
-				<div class="prestations-card">
+				<div class="prestations-card prestation-slide">
 					<div class="prestation-card-top">
-							<h3>Ingénierie financière</h3>
-							<p>Tarif : <span>2 000 € par formation</span></p>
-						<?php if (isset($_SESSION['user_id'])) { ?>
-							<form action="../checkout-session.php?formation=Ingénierie financière" method="POST">
-								<button type="submit">Acheter cette prestation</button>
-							</form>
-							<form action="../create-quote.php?formation=<?=$row['formation']?>" method="POST">
-								<button type="submit">Obtenir un devis</button>
-							</form>
-						<?php } else { ?>
-							<form action="login-signup.php" method="POST">
-								<button type="submit">Acheter cette prestation</button>
-							</form>
-							<form action="login-signup.php" method="POST">
-								<button type="submit">Obtenir un devis</button>
-							</form>
-						<?php } ?>
+						<h3>Ingénierie financière</h3>
+						<p>Tarif : <span>2 000 € par formation</span></p>
+						<div class="prestation-top-btns">
+							<?php if (isset($_SESSION['user_id'])) { ?>
+								<form action="../checkout-session.php?link=inge" method="POST">
+									<button type="submit">Acheter cette prestation</button>
+								</form>
+								<form action="../create-quote.php?formation=inge" method="POST">
+									<button type="submit">Obtenir un devis</button>
+								</form>
+							<?php } else { ?>
+								<form action="login-signup.php" method="POST">
+									<button type="submit">Acheter cette prestation</button>
+								</form>
+								<form action="login-signup.php" method="POST">
+									<button type="submit">Obtenir un devis</button>
+								</form>
+							<?php } ?>
+						</div>
 					</div>
 					<div class="prestation-card-text">
-						<p></p>
+						<p>Formablocs vous accompagne pour identifier vos charges fixes, vos charges variables, vos revenus afin d’en ressortir un seuil de rentabilité pour chaque action de formation et dégager des revenus. Nous vous proposons de co-cpnstruire avec vous, votre grille de coût personalisée pour une meilleure gestion financière de vos actions.</p>
+					</div>
+					<div class="carousel-dots">
+						<span></span>
+						<span class="active-dot"></span>
 					</div>
 				</div>
 
-				<div class="finance-carousel-right-arrow">
+				<div class="prestation-right-arrow">
 					<img src="<?=$domain?>/icons/FLECHE DROITE BLEUE.svg" alt="Suivant" id="next-1">
+				</div>
+
+			</div>
+			<div class="prestations-carousel">
+
+				<div class="prestation-left-arrow">
+					<img src="<?=$domain?>/icons/FLECHE GAUCHE BLEUE.svg" alt="Précedent" id="prev-2">
+				</div>
+
+				<div class="prestations-card prestation2-slide current-slide">
+					<div class="prestation-card-top">
+							<h3>Qualité</h3>
+							<p>Tarif : <span>1 500 à 4 000 € selon la taille de l'organisme</span></p>
+
+							<div class="prestation-top-btns">
+								<?php if (isset($_SESSION['user_id'])) { ?>
+									<form action="../checkout-session.php?link=quality" method="POST">
+										<button type="submit">Acheter cette prestation</button>
+									</form>
+									<form action="../create-quote.php?link=quality" method="POST">
+										<button type="submit">Obtenir un devis</button>
+									</form>
+								<?php } else { ?>
+									<form action="login-signup.php" method="POST">
+										<button type="submit">Acheter cette prestation</button>
+									</form>
+									<form action="login-signup.php" method="POST">
+										<button type="submit">Obtenir un devis</button>
+									</form>
+								<?php } ?>
+							</div>
+					</div>
+					<div class="prestation-card-text">
+						<p>Les différentes politique de l’emploi et de la formation imposent, aux organismes de formation, des critères de qualité de plus en plus exigeants. FormaBlocs vous accompagne pour la mise en conformité de votre structure en vu de l’obtention du certificat qualité Qualiopi.</p>
+					</div>
+					<div class="carousel-dots">
+						<span class="active-dot"></span>
+						<span></span>
+					</div>
+				</div>
+
+				<div class="prestations-card prestation2-slide">
+					<div class="prestation-card-top">
+						<h3>Bilan pédagogique</h3>
+						<p>Tarif : <span>900 €</span></p>
+						<div class="prestation-top-btns">
+							<?php if (isset($_SESSION['user_id'])) { ?>
+								<form action="../checkout-session.php?link=bilan" method="POST">
+									<button type="submit">Acheter cette prestation</button>
+								</form>
+								<form action="../create-quote.php?link=bilan" method="POST">
+									<button type="submit">Obtenir un devis</button>
+								</form>
+							<?php } else { ?>
+								<form action="login-signup.php" method="POST">
+									<button type="submit">Acheter cette prestation</button>
+								</form>
+								<form action="login-signup.php" method="POST">
+									<button type="submit">Obtenir un devis</button>
+								</form>
+							<?php } ?>
+						</div>
+					</div>
+					<div class="prestation-card-text">
+						<p>Formablocs vous accompagne pour identifier vos charges fixes, vos charges variables, vos revenus afin d’en ressortir un seuil de rentabilité pour chaque action de formation et dégager des revenus. Nous vous proposons de co-cpnstruire avec vous, votre grille de coût personalisée pour une meilleure gestion financière de vos actions.</p>
+					</div>
+					<div class="carousel-dots">
+						<span></span>
+						<span class="active-dot"></span>
+					</div>
+				</div>
+
+				<div class="prestation-right-arrow">
+					<img src="<?=$domain?>/icons/FLECHE DROITE BLEUE.svg" alt="Suivant" id="next-2">
+				</div>
+
+			</div>
+			<div class="prestations-carousel">
+
+				<div class="prestation-left-arrow">
+					<img src="<?=$domain?>/icons/FLECHE GAUCHE BLEUE.svg" alt="Précedent" id="prev-3">
+				</div>
+
+				<div class="prestations-card prestation3-slide current-slide">
+					<div class="prestation-card-top">
+							<h3>RNCP</h3>
+							<p>Tarif : <span>1 500 € par diplôme</span></p>
+
+							<div class="prestation-top-btns">
+								<?php if (isset($_SESSION['user_id'])) { ?>
+									<form action="../checkout-session.php?link=RNCP" method="POST">
+										<button type="submit">Acheter cette prestation</button>
+									</form>
+									<form action="../create-quote.php?link=RNCP" method="POST">
+										<button type="submit">Obtenir un devis</button>
+									</form>
+								<?php } else { ?>
+									<form action="login-signup.php" method="POST">
+										<button type="submit">Acheter cette prestation</button>
+									</form>
+									<form action="login-signup.php" method="POST">
+										<button type="submit">Obtenir un devis</button>
+									</form>
+								<?php } ?>
+							</div>
+					</div>
+					<div class="prestation-card-text">
+						<p>Proposer des titres enregistrés au RNCP c’est favorisé les possibilités de financement de ses formations. La démarche de reconnaissance est RNCP suit une méthodologie complexe. FormaBlocs peut vous accompagner.</p>
+					</div>
+					<div class="carousel-dots">
+						<span class="active-dot"></span>
+						<span></span>
+					</div>
+				</div>
+
+				<div class="prestations-card prestation3-slide">
+					<div class="prestation-card-top">
+						<h3>Ingénierie pédagogique </h3>
+						<p>Tarif : <span>40€/h</span></p>
+						<div class="prestation-top-btns">
+							<a href="contact.php"><button>Nous contacter</button></a>
+						</div>
+					</div>
+					<div class="prestation-card-text">
+						<p>Notre experte, Mme Zitouni, a derrière elle 7 années d’expérience dans l’accompagnement à la reconversion, la gestion d’organismes de formation et la pédagogie. Elle peut vous accompagner dans l’ingénierie pédagogique de vos actions de formation. N’hésitez pas à nous faire part de vos besoins ou difficultés afin que l’on puisse vous faire une proposition personnalisée.</p>
+					</div>
+					<div class="carousel-dots">
+						<span></span>
+						<span class="active-dot"></span>
+					</div>
+				</div>
+
+				<div class="prestation-right-arrow">
+					<img src="<?=$domain?>/icons/FLECHE DROITE BLEUE.svg" alt="Suivant" id="next-3">
 				</div>
 
 			</div>
@@ -95,6 +244,8 @@ require_once (__DIR__ . '/includes/header.php');
 	</div>
 </section>
 
+
+<script src="<?=$domain?>/js/carousel-prestations.js"></script>
 <?php
 
 require_once (__DIR__ . '/includes/footer.php');
